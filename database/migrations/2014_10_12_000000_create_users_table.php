60caaa27ca7f676c8cff5d73ac7eaa8e0f6774d7
Hiago Laureano <hiagolaureano@hotmail.com>
2024-08-30 23:04:17 -0300

    added superuser and active columns to users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean("superuser")->default(false);
+            $table->boolean("active")->default(true);
             $table->timestamps();
         });
     }
